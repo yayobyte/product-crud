68dfaff2cc8004b3f5b1d66a6b092a93bea89b9d
Yayo Gutierrez <yayobyte@gmail.com>
2025-05-03 22:54:54 -0500

    Error handler and refactor server file

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import productRoutes from './routes/products';
 import { loadInitialProducts } from './data/products';
@@ -10,17 +10,30 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
+app.get('/', rootHandler);
 
-app.get('/', (req: Request, res: Response) => {
+app.use(errorHandler);
+
+startServer();
+
+function rootHandler(req: Request, res: Response) {
   res.send('Backend server is running!');
-});
+}
 
-const startServer = async () => {
+function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.error('Unhandled error:', err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+}
+
+async function startServer() {
   await loadInitialProducts();
   console.log('Initial products loaded successfully.');
   app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
   });
-};
-
-startServer();
+}
