91689e8b530600e28a03e5dea18dce97a17104b0
Yayo Gutierrez <yayobyte@gmail.com>
2025-05-03 19:24:40 -0500

    get product api mocked

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
+import productRoutes from './routes/products'; // Import the product router
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend server is running!');
