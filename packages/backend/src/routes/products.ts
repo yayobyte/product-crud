44ac3d03bb366f36977902bc574f1b005677e36a
Yayo Gutierrez <yayobyte@gmail.com>
2025-05-03 20:41:28 -0500

    Mock the products once when calling api/products

--- packages/backend/src/routes/products.ts
+++ packages/backend/src/routes/products.ts
@@ -1,44 +1,25 @@
 import express, { Router, Request, Response } from 'express';
+import axios from 'axios';
 import { Product } from '../types/product';
 
 const router: Router = express.Router();
 
-let mockProducts: Product[] = [
-  {
-    id: 1,
-    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    price: 109.95,
-    description:
-      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-    rating: { rate: 3.9, count: 120 },
-  },
-  {
-    id: 2,
-    title: 'Mens Casual Premium Slim Fit T-Shirts ',
-    price: 22.3,
-    description:
-      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: "men's clothing",
-    image:
-      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: { rate: 4.1, count: 259 },
-  },
-  {
-    id: 3,
-    title: 'Mens Cotton Jacket',
-    price: 55.99,
-    description:
-      'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
-    rating: { rate: 4.7, count: 500 },
-  },
-];
+let products: Product[] = [];
+const FAKE_STORE_URL = 'https://fakestoreapi.com/products';
 
-router.get('/', (req: Request, res: Response) => {
-  res.send(mockProducts);
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    if (products.length === 0) {
+      console.log('Fetching initial products from FakeStoreAPI...');
+      const response = await axios.get<Product[]>(FAKE_STORE_URL);
+      products = response.data;
+      console.log(`Fetched ${products.length} products.`);
+    }
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ message: 'Failed to fetch products' });
+  }
 });
 
 export default router;
